55843ca2e165eb99f011bc8a456df6d0297c58b6
s1lv3rrud <lek8575@naver.com>
2024-04-02 23:29:25 +0900

    style: 글자색 변경

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import { ToDoList } from "./todolist/page";
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-4xl font-bold text-center my-8">'s todolist</h1>
+      <h1 className="text-white text-4xl font-bold text-center my-8">
+        's todolist
+      </h1>
       <ToDoList />
     </div>
   );
