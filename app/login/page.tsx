52c7c8a36b8b05baad987de1fbb3650b2e910b07
s1lv3rrud <lek8575@naver.com>
2024-04-03 21:19:10 +0900

    style: login page button 위치 변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -62,12 +62,14 @@ const Login = () => {
               onChange={handleChange}
             />
           </div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2"
-          >
-            Login
-          </button>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2"
+            >
+              Login
+            </button>
+          </div>
         </form>
         <div className="text-center">
           <Link href="/join" legacyBehavior>
