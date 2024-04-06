923bf3b3cdeebda506ebbee5e49b23f9ab4c80e3
s1lv3rrud <lek8575@naver.com>
2024-04-06 23:25:27 +0900

    style: 로그인 버튼 추가

--- app/todolist/page.tsx
+++ app/todolist/page.tsx
@@ -129,7 +129,12 @@ export const ToDoList = () => {
           </li>
         ))}
       </ul>
-      <div className="mt-4 flex justify-center items-center">
+      <div className="mt-4 flex justify-center items-center space-x-4">
+        <Link href="/login" legacyBehavior>
+          <a className="inline-block text-blue-700 bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2">
+            Login
+          </a>
+        </Link>
         <Link href="/join" legacyBehavior>
           <a className="inline-block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2">
             Join
